a0dd3654ae3aaad66aabc0bb4768678818622eb1
secretaria de radio chivilcoy <casaresveronica54@gmail.com>
2023-08-25 16:52:58 -0300

    login-con-github

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,11 +1,66 @@
 import passport from "passport";
 import local from "passport-local";
 import User from "../models/user.model.js";
+import GitHubStrategy from "passport-github2";
 import { createHash,isValidPassword } from "../utils.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const LocalStrategy = local.Strategy;
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+const GITHUB_CALLBACK_URL = process.env.GITHUB_CALLBACK_URL;
 
 const initializePassport = () => {
+  passport.use(
+    "github",
+    new GitHubStrategy(
+      {
+        clientID: GITHUB_CLIENT_ID,
+        clientSecret: GITHUB_CLIENT_SECRET,
+        callbackURL: GITHUB_CALLBACK_URL,
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        console.log(profile)
+        try {
+          let user = await User.findOne({
+            email: profile?.emails[0]?.value,
+          });
+          if (!user) {
+            const newUser = {
+              first_name: profile.displayName.split(" ")[0],
+              last_name: profile.displayName.split(" ")[1],
+              email: profile?.emails[0]?.value,
+              age: 18,
+              password: crypto.randomBytes(20).toString("hex"),
+            };
+            let result = await User.create(newUser);
+            done(null, result);
+          } else {
+            done(null, user);
+          }
+        } catch (err) {
+          done(err, null);
+        }
+      }
+    )
+  );
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      let user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
+  });
+};
+
+
     //estrategia para el registro
     passport.use(
       "register",
@@ -84,7 +139,7 @@ const initializePassport = () => {
           }
         )
       );
-    };
+    
     
   
   export default initializePassport;
--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -144,4 +144,21 @@ router.get("/failRegister", async (req, res) => {
 //     });
 //   }
 // });
+
+//ruta para registare con github
+router.get(
+  "/github",
+  passport.authenticate("github", { scope: ["user:email"] }),
+  async (req, res) => {}
+);
+
+router.get(
+  "/githubcallback",
+  passport.authenticate("github", { failureRedirect: "/" }),
+  async (req, res) => {
+    req.session.user = req.user;
+    req.session.admin = true;
+    res.redirect("/api/products");
+  }
+);
 export default router;
