e35aca46ea54a31d56a71f41a008e1a72de511e9
Veronica Casarez <casaresveronica54@gmail.com>
2023-08-25 10:38:41 -0300

    desafio-scrypt-passport-github

---
+++ public/js/login.js
@@ -0,0 +1,26 @@
+async function postLogin(username, password) {
+  const response = await fetch("/api/session/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+  console.log(response)
+  const result = await response.json(); 
+
+  if (result.respuesta === "Autenticado exitosamente") {
+    window.location.href = "/api/products"; 
+
+  } else {
+    console.log(result); // Muestra los datos en caso de error
+  }
+}
+
+const loginForm = document.getElementById("login-form");
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  postLogin(username, password);
+});
---
+++ src/utils.js
@@ -0,0 +1,18 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
+
+
+
+//logica para hashear la contraseña
+export const createHash = (password) =>
+  bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+  //logica para comparar la contraseña sin hashear con la que esta en la base de datos
+  //devuelve true o false
+export const isValidPassword = (savedPassword, password) => {
+  console.log({ "cloud password": savedPassword, loginPassword: password });
+  return bcrypt.compareSync(password, savedPassword);
+};
